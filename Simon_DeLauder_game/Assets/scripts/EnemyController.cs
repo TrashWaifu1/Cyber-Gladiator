b931b20eac4dc5090746705aaca7b3810aac63d4
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-04-28 10:28:06 -0700

    Uploading Game
    In this update I Uploaded the game files

---
+++ Simon_DeLauder_game/Assets/scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject target;
+    bool isFolowing;
+    Rigidbody2D rb;
+    Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = rb.velocity;
+
+        if (target.transform.position.x < transform.position.x && isFolowing)
+        {
+            velocity.x = -5f;
+        }
+        else if (isFolowing)
+        {
+            velocity.x = 5f;
+        }
+
+        rb.velocity = velocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            isFolowing = true;
+        }
+        else
+        {
+            isFolowing = false;
+        }
+    }
+}
---
+++ Simon_DeLauder_game/Assets/scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5;
+    public float jumpHight = 5;
+    public int Health = 3;
+    public int maxHealth = 3;
+    public Vector2 velocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = rb.velocity;
+
+
+        velocity.x = Input.GetAxisRaw("Horizontal") * speed;
+
+
+        if (Input.GetKey(KeyCode.Space) && Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.52f), Vector2.down, 0.01f))
+            velocity.y = jumpHight;
+
+        rb.velocity = velocity;
+
+        if (Health == 0)
+        {
+            transform.position = new Vector2(0, 0);
+            Health = maxHealth;
+        }
+    }
+}
