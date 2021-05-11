4423bb1bd562f01c4b16837cddceb9416c53cef9
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-05-11 16:19:48 -0700

    Built the gun follow mouse

--- Simon_DeLauder_game/Assets/scripts/Weapon.cs
+++ Simon_DeLauder_game/Assets/scripts/Weapon.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    Vector2 mousePos;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
