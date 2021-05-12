0c8b49145b4ccc053da4ef6d9baa7d5aa95595e1
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-05-11 18:28:23 -0700

    optimization weapons script
    migrated everything in weapon script to the player controller and attempted to do bug fixing

--- Simon_DeLauder_game/Assets/scripts/PlayerController.cs
+++ Simon_DeLauder_game/Assets/scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D weaponJointRB;
+    public GameObject weaponJoint;
+    public Camera cam;
     public Rigidbody2D rb;
     public float speed = 5;
     public float jumpHight = 5;
@@ -11,7 +14,7 @@ public class PlayerController : MonoBehaviour
     public int maxHealth = 3;
     public Vector2 velocity;
 
-
+    Vector2 mousePos;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,11 +24,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 lookDir = mousePos - weaponJointRB.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+        weaponJointRB.rotation = angle;
+        //weaponJoint.transform.SetPositionAndRotation.
+
         velocity = rb.velocity;
-
-
         velocity.x = Input.GetAxisRaw("Horizontal") * speed;
-
 
         if (Input.GetKey(KeyCode.Space) && Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y - 0.52f), Vector2.down, 0.01f))
             velocity.y = jumpHight;
