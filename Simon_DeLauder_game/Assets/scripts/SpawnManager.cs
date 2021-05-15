3e3e30200651b1a2907d80f7cb5947ed3ac60edd
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-05-15 16:28:35 -0700

    Added GameManager and SpawnManager
    I also add a little bit to the spawnManager code

---
+++ Simon_DeLauder_game/Assets/scripts/SpawnManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public int roundNum;
+    public GameObject[] enemys;
+    public GameObject[] spawners;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
