d6fa9e8c1c03af72cd11c2719f59504a1dc09e41
TrashWaifu1 <82532109+TrashWaifu1@users.noreply.github.com>
2021-05-15 19:50:19 -0700

    Finished spawn manager script
    and I moved other stuff around in my sample scene

--- Simon_DeLauder_game/Assets/scripts/SpawnManager.cs
+++ Simon_DeLauder_game/Assets/scripts/SpawnManager.cs
@@ -5,36 +5,54 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public int roundNum = 1;
-    public int enemysOut;
+    public int enemysOut = 0;
     public GameObject[] enemys;
     public GameObject[] spawners;
-    public int roundStatis;
+    public int roundStatus = 0;
 
     int randomSpawner;
     int enemyType;
     int maxEnemysOut = 30;
     int enemysRoundCount = 1;
-    int enemysToBeDeploid;
+    int enemysToBeDeploid = 1;
 
-    
     void Update()
     {
-        if (roundStatis == 3)
+        switch (roundStatus)
         {
-            roundNum++;
-            enemysRoundCount += 5;
-            enemysToBeDeploid = enemysRoundCount;
-            roundStatis = 2;
+                //Standby Phase
+            case 2:
+                if (enemysOut == 0)
+                    roundStatus = 1;
+                break;
+                //Reset Phase
+            case 1:
+                roundReset();
+                break;
+                //Spawning Phase
+            default:
+                if (enemysOut < maxEnemysOut)
+                {
+                    spawn();
+                    if (enemysToBeDeploid == 0)
+                        roundStatus = 2;
+                }
+                break;
         }
+    }
 
-        if (roundStatis == 2)
-        {
-            
-        }
+    void roundReset()
+    {
+        roundNum++;
+        enemysRoundCount += 5;
+        enemysToBeDeploid = enemysRoundCount;
+        roundStatus = 0;
     }
 
     void spawn()
     {
+        enemysOut++;
+        enemysToBeDeploid--;
         randomSpawner = Random.Range(0, spawners.Length);
         enemyType = Random.Range(0, enemys.Length);
         Instantiate(enemys[enemyType], spawners[randomSpawner].transform);
